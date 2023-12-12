c13ce62a72ae0a22acd83e50cc4484f3d90e5428
Labinot Pajaziti <38728474+Labinot98@users.noreply.github.com>
2023-12-12 21:14:05 +0100

    First commit

--- Package.swift
+++ Package.swift
@@ -5,19 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "PasswordStrengthIndicator",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11),
+        .watchOS(.v7),
+        .tvOS(.v14)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "PasswordStrengthIndicator",
-            targets: ["PasswordStrengthIndicator"]),
+            targets: ["PasswordStrengthIndicator"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         .target(
-            name: "PasswordStrengthIndicator"),
+            name: "PasswordStrengthIndicator",
+            dependencies: []
+        ),
         .testTarget(
             name: "PasswordStrengthIndicatorTests",
-            dependencies: ["PasswordStrengthIndicator"]),
+            dependencies: ["PasswordStrengthIndicator"]
+        ),
     ]
 )
+
--- Sources/PasswordStrengthIndicator/PasswordStrengthIndicator.swift
+++ Sources/PasswordStrengthIndicator/PasswordStrengthIndicator.swift
@@ -1,2 +1,58 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct PasswordStrengthView: View {
+    @Binding var password: String
+
+    public init(password: Binding<String>) {
+        self._password = password
+    }
+
+    public var body: some View {
+        VStack(alignment: .leading) {
+            Text("Password Strength:")
+                .font(.headline)
+                .foregroundColor(.black)
+
+            ProgressBar(passwordStrength: calculateStrength(password))
+                .frame(height: 10)
+        }
+    }
+
+    private func calculateStrength(_ password: String) -> Double {
+        // Logic to determine password strength (you can implement your own here)
+        let passwordLength = Double(password.count)
+        return min(passwordLength / 10.0, 1.0) // Adjust this according to your strength criteria
+    }
+}
+
+struct ProgressBar: View {
+    var passwordStrength: Double
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .foregroundColor(Color.gray.opacity(0.3))
+                    .cornerRadius(5.0)
+
+                Rectangle()
+                    .frame(width: min(CGFloat(self.passwordStrength) * geometry.size.width, geometry.size.width), height: geometry.size.height)
+                    .foregroundColor(self.getPasswordStrengthColor())
+                    .cornerRadius(5.0)
+                    .animation(.linear)
+            }
+        }
+    }
+
+    private func getPasswordStrengthColor() -> Color {
+        // Modify this logic to set colors based on the password strength
+        if passwordStrength < 0.3 {
+            return Color.red
+        } else if passwordStrength < 0.7 {
+            return Color.yellow
+        } else {
+            return Color.green
+        }
+    }
+}
